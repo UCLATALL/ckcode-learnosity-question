89a26f8a1e795d1c1ae69a87eabc73d92a223905
Adam Blake <adamblake@g.ucla.edu>
2021-11-16 13:40:22 -0800

    Add iframe width styling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ LearnosityAmd.define(() => {
    * @param {{$el: JQuery, events: { trigger: (name: string) => void }}} init
    */
   function CKCodeFeature(init) {
-    init.$el.html(`<iframe src="https://uclatall.github.io/ckcode-sandbox">`);
+    const style = ["min-width:600px", "max-width:900px"].join(";");
+    init.$el.html(
+      `<iframe src="https://uclatall.github.io/ckcode-sandbox" style="${style}">`
+    );
     init.events.trigger("ready");
   }
 
